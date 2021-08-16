cb1743fb4af2bd2a6c1889aea664cf231839276b
Spencer Foote <83970495+SJFoote@users.noreply.github.com>
2021-08-16 10:42:04 -0700

    updated

---
+++ hackerRank.py
@@ -0,0 +1,31 @@
+
+# def greaterThanSecond(arr):
+#     if len(arr) < 2:
+#         return False
+
+#     newArr = []
+#     for i in range(0, len(arr), 1):
+#         if i < 1 or i >= 2:
+#             newArr.append(arr[i])
+
+#     return newArr
+
+# # test cases
+# print(greaterThanSecond([1, 3, 5, 7]))
+# print(greaterThanSecond([0, -3, 2, 5]))
+# print(greaterThanSecond([0, 1, 2, -5]))
+# print(greaterThanSecond([]))
+# print(greaterThanSecond([2]))
+
+
+def printXtoY(x, y):
+    for i in range(x, y, 1):
+        print(i)
+
+
+printXtoY(1,10)
+
+printXtoY(10,20)
+
+printXtoY(-20,-10) 
+
