9f99985b5dac13f4b604b4071e5cbae9e4358320
Spencer Foote <83970495+SJFoote@users.noreply.github.com>
2021-08-18 10:15:16 -0700

    bleep bloop update

--- bubbleSort.py
+++ bubbleSort.py
@@ -86,18 +86,27 @@
 # print(factorial(8))
 
 # bubble sort practice
-def bubble(arr):
-    for i in range(0, len(arr)-1, 1):
-        # iterate through list, take current value and compare it to the rest of the list
-        for i in range(0, len(arr)-1, 1):
-            # store first value in comp, this is the comparison variable
-            if arr[i] > arr[i+1]:
-                temp = arr[i+1]
-                arr[i+1] = arr[i]
-                arr[i] = temp
-        print(arr)
+# def bubble(arr):
+#     for i in range(0, len(arr)-1, 1):
+#         # iterate through list, take current value and compare it to the rest of the list
+#         for i in range(0, len(arr)-1, 1):
+#             # store first value in comp, this is the comparison variable
+#             if arr[i] > arr[i+1]:
+#                 temp = arr[i+1]
+#                 arr[i+1] = arr[i]
+#                 arr[i] = temp
+#         print(arr)
 
-bubble([9, 4, 2, 10, 1, 2])
+# bubble([9, 4, 2, 10, 1, 2])
 
 # no use recursion to solve bubble sort!!!!
+
+def sum(n):
+    if n <= 1:
+        return n
+    print(n)
+    return n + sum(n-1)
+
+print(sum(5))
+    
 
