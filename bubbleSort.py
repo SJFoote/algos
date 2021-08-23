1a6a78ac0e55f0cc5a8c23cb2e5d11609dea230b
Spencer Foote <83970495+SJFoote@users.noreply.github.com>
2021-08-23 08:50:24 -0700

    bloop bloop leetcode added bleep bleep

--- bubbleSort.py
+++ bubbleSort.py
@@ -158,32 +158,33 @@
 #         return " -> ".join(nodes)
         
 
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
+# class Node:
+#     def __init__(self, data):
+#         self.data = data
+#         self.next = None
 
-class LinkedList:
-    def __init__(self):
-        self.head = None
+# class LinkedList:
+#     def __init__(self):
+#         self.head = None
 
-    def printList(self):
-        temp = self.head
-        while(temp):
-            print(temp.data)
-            temp = temp.next
+#     def printList(self):
+#         temp = self.head
+#         while(temp):
+#             print(temp.data)
+#             temp = temp.next
 
-if __name__=='__main__':
-    llist = LinkedList()
+# if __name__=='__main__':
+#     llist = LinkedList()
 
-    llist.head = Node(1)
-    second = Node(2)
-    third =  Node(3)
+#     llist.head = Node(1)
+#     second = Node(2)
+#     third =  Node(3)
 
-    llist.head.next = second;
-    second.next = third;
+#     llist.head.next = second;
+#     second.next = third;
 
-list1 = Node(4)
+# list1 = Node(4)
 
-print(list1)
+# print(list1)
+
 
---
+++ leetCode.py
@@ -0,0 +1,17 @@
+# from leet code
+# nums = [2,7,11,15]
+
+# target = 9
+
+# def twoSum(nums, target):
+#     for i in range(0, len(nums), 1):
+#         var1 = nums[i]
+#         for j in range(0, len(nums)-1, 1):
+#             sum = var1 + nums[j+1]
+#             print(var1)
+#             print(nums[j])
+#             if sum == target:
+#                 print(var1, nums[j])
+#                 return sum
+
+# print(twoSum(nums, target))
