65f3515d8cb738d284a2fbc2afb439246aa0a7f3
Spencer Foote <83970495+SJFoote@users.noreply.github.com>
2021-09-10 10:32:14 -0700

    bleep bleep progress

--- hackerrank.js
+++ hackerrank.js
@@ -33,12 +33,34 @@
 // For example, factorial(3) = 6 (or 123); factorial(5) = 120 (or 12345).
 // For this challenge, do not worry about time-complexity of your code yet.
 
-function factorial(n) {
-    if (n == 1){
-        return;
+// function factorial(n) {
+//     if (n == 1){
+//         return;
+//     } else {
+//         return n * factorial(n - 1);
+//     }
+// }
+
+// factorial(3)
+
+// ****
+
+// Write a program that reverses a string using recursion. Given the string "freeCodeCamp" your program should return "pmaCedoCeerf".
+function reverse(str){
+
+    if (str.length == 0){
+        return str
     } else {
-        return n * factorial(n - 1);
+        return str[str.length-1] + reverse(str.substr(0, str.length-1))
     }
 }
 
-factorial(3)
+console.log(reverse('freeCodeCamp'))
+// console.log(reverse('freeCodeCamp'))
+
+// ****
+
+// Write a program that returns the number of times a character appears in string. Your program should receive a string and the character. It should then return number of times the character appears in the string.
+// Given the string "JavaScript" and a character "a", your program should return 2.
+
+// Hint: Try to figure out when you want the function to stop calling itself and how to return a smaller version of the problem every time the function calls itself.
